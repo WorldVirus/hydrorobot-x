85593fca0e4f2d1abac131b4a5136698010a3d4c
WorldVirus <metallika956@gmail.com>
2018-04-16 19:29:58 +0300

    heroku shit

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('*', function(request, response) {
     response.sendFile(__dirname + '/public/index.html');
